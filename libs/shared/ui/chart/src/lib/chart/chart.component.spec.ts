5dff33b02dfb94f860dc09cd400302981458d60d
Apurwa Bhat <Apurwa.Bhat@ad.infosys.com>
2020-02-17 14:32:20 +0530

    Task1A code changes

--- libs/shared/ui/chart/src/lib/chart/chart.component.spec.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ChartComponent } from './chart.component';
+import { GoogleChartsModule } from 'angular-google-charts';
+import { CHART_MOCK_DATA } from './mocks/chart-data-mock.spec';
+import { of } from 'rxjs/internal/observable/of';
+import { Subscription } from 'rxjs';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -8,18 +11,42 @@ describe('ChartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChartComponent ]
-    })
-    .compileComponents();
+      imports: [GoogleChartsModule.forRoot()],
+      declarations: [ChartComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChartComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit()', () => {
+    it('should set chart data when "data$" is empty', () => {
+      component.data$ = of([]);
+      component.ngOnInit();
+      expect(component.chartData).toEqual(CHART_MOCK_DATA);
+    });
+    it('should set chart data when "data$" is not empty', () => {
+      component.data$ = of([['AAPL', 'One Month']]);
+      component.ngOnInit();
+      expect(component.chartData.data).toEqual([['AAPL', 'One Month']]);
+    });
+  });
+
+  describe('ngOnDestroy()', () => {
+    it('should unsubscribe from stocks data', () => {
+      (component as any).chartDataSubscription = new Subscription();
+      const unsubscribeSpy: jasmine.Spy = spyOn(
+        (component as any).chartDataSubscription,
+        'unsubscribe'
+      ).and.stub();
+      component.ngOnDestroy();
+      expect(unsubscribeSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
---
+++ libs/stocks/feature-shell/src/lib/stocks/enums/stocks-labels.enum.ts
@@ -0,0 +1,6 @@
+/**
+ * Stock Labels
+ */
+export enum EStocksLabels {
+    TIME_PERIOD = 'Favorite time period'
+}
